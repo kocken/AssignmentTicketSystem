eb4cad1e938e3b389e4d41664348a65fdba569f8
Mikael <mikaelkarlgren@hotmail.se>
2018-05-01 16:09:40 +0200

    Added (client) web MVC project & changed footers

--- src/WebShop/Controllers/HomeController.cs
+++ src/WebShop/Controllers/HomeController.cs
@@ -4,14 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AdminWebPanel.Models;
+using WebShop.Models;
 
-namespace AdminWebPanel.Controllers
+namespace WebShop.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
             return View();
         }
 
